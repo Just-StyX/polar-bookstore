f312e8b9134b0cc2b3c045b8fe0ddb12d1b594bb
Just-StyX <fjaccobian@gmail.com>
2025-05-26 02:21:29 -0400

    edge service update

--- cloud-services/edge-service/src/test/java/jsl/group/edge_service/EdgeServiceApplicationTests.java
+++ cloud-services/edge-service/src/test/java/jsl/group/edge_service/EdgeServiceApplicationTests.java
@@ -3,7 +3,7 @@ package jsl.group.edge_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.cloud.config.enabled=false"})
 class EdgeServiceApplicationTests {
 
 	@Test
